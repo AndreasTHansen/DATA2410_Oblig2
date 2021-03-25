c2388066c48966be0b4d6c4c3f36066f68e0c2ea
AndreasTHansen <andreas-torres@hotmail.com>
2021-03-25 15:26:53 +0100

    Merged master and deployment branch

--- server.py
+++ server.py
@@ -2,3 +2,26 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
+api = Api(app)
+
+users = {}
+
+
+def abort_if_not_exists(some_id, some_dict: dict, abort_message: str):
+    if some_id not in some_dict:
+        abort(404, message=abort_message)
+
+
+def abort_if_exists(some_id, some_dict: dict, abort_message: str):
+    if some_id in some_dict:
+        abort(409, message=abort_message)
+
+
+class User(Resource):
+    def get(self, user_id):
+        if user_id:
+            pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

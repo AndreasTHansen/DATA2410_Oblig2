d32bd39822ca8d32555bd5a562722efa06a58eb4
Uy Quoc Nguyen <s341864@oslomet.no>
2021-03-27 00:50:05 +0100

    Testing has been performed for all classes except Messages. Messages has not been fully implemented yet. Some feature and checks is still missing.

--- main.py
+++ main.py
@@ -1,81 +1,122 @@
-import requests
-import json
-from datetime import datetime
-from time import sleep
+from requests import get, post, delete
+from server import Users, ChatRooms, RoomUsers, Messages
+from random import choice
 
-route = "http://127.0.0.1:5000/api/rooms/"
 
-users = {
-    'uyqn': {
-        'Name': "Uy Quoc Nguyen",
-        'Age': 29,
-        'Email': "uy.nguyen92@gmail.com"
-    },
-    'panders': {
-        'Name': "Anders Hagan Ottersland",
-        'Age': 21,
-        'Email': "anders@epost.no"
-    },
-    'torresso': {
-        'Name': "Andreas Torres Hansen",
-        'Age': 21,
-        'Email': "andreas@google.com"
+def route(resource, user_id: str = None, room_id: str = None) -> str or None:
+    resource = resource.__name__.lower()
+    url = "http://127.0.0.1:5000"
+    routes = {
+        'users':
+            f"/api/user" + ('s' if user_id is None else f"/{user_id}"),
+        'chatrooms':
+            f"/api/room" + ('s' if room_id is None else f"/{room_id}"),
+        'roomusers':
+            f"/api/room/{room_id}/users",
+        'messages':
+            f"/api/room/{room_id}/" + ('messages' if user_id is None else f"{user_id}/messages")
     }
-}
 
-some_dict = {
-    'chicken': {
-        'Name': "Chicken wings",
-        'Size': 10,
-        'Users': {}
-    }
-}
+    result = url + routes[resource]
+    if result.__contains__("None"):
+        return None
+    return result
 
-some_dict['chicken']['Users'].update(users)
 
-users.update({'vader': {
-    'Name': 'Anakin Skywalker',
-    'Age': 45,
-    'Email': 'darth_vader@deathstar.com'
-}})
+def get_test(resource, user_id: str = None, room_id: str = None) -> None:
+    get_route = route(resource, user_id, room_id)
+    print(30 * "-")
+    print(f"/// GET test on {resource.__name__} with user_id = {user_id} and room_id = {room_id} ///")
 
-some_dict['chicken']['Users'].update({'vader': users['vader']})
+    if get_route is None:
+        raise IOError(f"Some parameters are missing to perform the required GET test")
+    print(f"route = {get_route}")
+    response = get(get_route)
+    print(f"{response.json()}, status code = {response.status_code}")
+    print(30 * "-")
 
-messages = {str(datetime.now()): {
-    'user': 'uyqn',
-    'room': 'chicken',
-    'timestamp': datetime.now().strftime("%c"),
-    'message': "I love chickens!"
-}}
 
-sleep(0.1)
+def post_test(resource, user_id: str = None, room_id: str = None, data: dict = None) -> None:
+    post_route = route(resource, user_id, room_id)
+    print(30 * "-")
+    print(f"/// POST test on {resource.__name__} with user_id = {user_id} and room_id = {room_id}")
 
-messages.update({
-    str(datetime.now()): {
-        'user': 'panders',
-        'room': 'chicken',
-        'time': datetime.now().strftime("%c"),
-        'message': "I love chicken too!"
-    }
-})
+    if post_route is None:
+        raise IOError(f"Some parameters are missing to perform the required POST test")
+    if resource != Users and resource != ChatRooms and data is None:
+        raise IOError(f"data is required for {resource.__name__}")
+    print(f"route = {post_route}")
+    if data is not None:
+        print(f"data = {str(data)}")
+    response = post(post_route, data)
+    print(f"{response.json()}, status code = {response.status_code}")
+    print(30 * "-")
 
-sleep(0.1)
 
-messages.update({
-    str(datetime.now()): {
-        'user': 'torresso',
-        'room': 'chicken',
-        'time': datetime.now().strftime("%c"),
-        'message': "Let's go grab some chicken then!"
-    }
-})
+def delete_test(resource, user_id: str = None, room_id: str = None) -> None:
+    delete_route = route(resource, user_id, room_id)
+    print(30 * "-")
+    print(f"/// DELETE test on {resource.__name__} with user_id = {user_id} and room_id = {room_id} ///")
 
-filtered = list(messages.values())
-filter2 = filter(lambda m: m['user'] == 'panders', filtered)
+    if delete_route is None:
+        raise IOError(f"Some parameters are missing to perform the required DELETE test")
+    print(f"route = {delete_route}")
+    response = delete(delete_route)
+    print(f"{response.json()}, status code = {response.status_code}")
+    print(30 * "-")
 
-for x in filter2:
-    print(x)
 
-#  response = requests.post(route+'chicken', {'Name': 'Chicken wings', 'Size': 20})
+def class_test(resource, user_ids: list = None, room_ids: list = None, datas: list = None) -> None:
+    if resource == Users:
+        if user_ids is None:
+            print(f"Cannot perform POST test because no user ids has been provided")
+        else:
+            for user in user_ids:
+                post_test(resource, user_id=user)
+            for user in user_ids:
+                post_test(resource, user_id=user)
+        for user in user_ids:
+            get_test(resource, user_id=user)
+        get_test(Users)
+        get_test(Users, user_id="whoswho")
 
-#  print(response.json())
+        for user in user_ids:
+            delete_test(resource, user_id=user)
+            get_test(resource, user_id=user)
+        delete_test(Users, user_id="whoswho")
+        get_test(Users)
+
+        if user_ids is not None:
+            for user in user_ids:
+                post_test(resource, user_id=user)
+
+    if resource == ChatRooms:
+        for room in room_ids:  # POST test
+            post_test(resource, room_id=room)
+        for room in room_ids:  # POST test on existing rooms
+            post_test(resource, room_id=room)
+        for room in room_ids:  # GET test on populated rooms
+            get_test(resource, room_id=room)
+        get_test(resource)
+        get_test(resource, room_id="whichswhich")
+
+    if resource == RoomUsers:
+        # Populate rooms and user_ids
+        for room in room_ids:
+            post_test(ChatRooms, room_id=room)
+        for user in user_ids:
+            post_test(Users, user_id=user)
+        for i in range(len(user_ids)):
+            room = choice(room_ids)
+            user = choice(user_ids)
+            post_test(resource, room_id=room, data={'user': user})
+        for room in room_ids:
+            get_test(resource, room_id=room)
+        get_test(ChatRooms)
+        get_test(Users)
+
+    if resource == Messages:
+        pass
+
+
+class_test(RoomUsers, user_ids=['uyqn', 'torresso', 'panders', 'vader', 'varys'], room_ids=['chicken', 'boar', 'pandora', 'group42'])
--- server.py
+++ server.py
@@ -8,125 +8,169 @@ api = Api(app)
 
 users = {}
 rooms = {}
-messages = {}
 
 json_indent = 1
 
 
-def abort_if_not_exists(some_id, some_dict: dict, abort_message: str):
-    if some_id not in some_dict:
+def abort_if_not_exists(some_id: str, iterable: iter, abort_message: str):
+    if some_id not in iterable:
         abort(404, message=abort_message)
 
 
-def abort_if_exists(some_id, some_dict: dict, abort_message: str):
-    if some_id in some_dict:
+def abort_if_exists(some_id: str, iterable: iter, abort_message: str):
+    if some_id in iterable:
         abort(409, message=abort_message)
 
 
-class User(Resource):
-    def get(self, user_id=None) -> (str, int):
+class Users(Resource):
+    def get(self, user_id: str = None) -> (str, int):
+        # Check if any user id has been provided
         if user_id:
-            abort_if_not_exists(user_id, users, f"Cannot find {user_id}")
-            return dumps(users[user_id], indent=json_indent), 200
-        return dumps(users, indent=json_indent), 200
+            if not user_id.strip():
+                return dumps({'message': f"User id is required to fetch a user"}, indent=json_indent), 200
+            # Check if specified user id exists
+            abort_if_not_exists(user_id, users, f"Cannot find user with user id \"{user_id}\"")
 
-    def post(self, user_id=None) -> (str, int):
+            # Return json format of user
+            return dumps(users[user_id], indent=json_indent), 200  # code 200 = OK
+
+        # If no user id has been provided return the whole list as json format
+        return dumps(users, indent=json_indent), 200  # code 200 = OK
+
+    def post(self, user_id: str = None) -> (str, int):
+        # If a user id has been provided check if it is blank/empty:
         if not user_id:
-            return f"User id required to post a new user", 400
-        if user_id:
-            abort_if_exists(user_id, users, f"A user with user id {user_id} already exists")
-            users[user_id] = reqparse.RequestParser() \
-                .add_argument('rooms', type=list, default=[]) \
-                .parse_args(strict=True)
-            return dumps(users[user_id], indent=json_indent), 201
+            return dumps({'message': "User id is required to create a new user"},
+                         indent=json_indent), 400  # bad request
+        if not user_id.strip():
+            return dumps({'message': "User id is required to create a new user"},
+                         indent=json_indent), 400  # bad request
 
-    def delete(self, user_id=None) -> (str, int):
+        # Check if the user already exists
+        abort_if_exists(user_id, users, f"A user with user id \"{user_id}\" already exists!")
+
+        # Collect necessary data to create a user
+        users[user_id] = reqparse.RequestParser() \
+            .add_argument('username', type=str, default=user_id) \
+            .add_argument('rooms', type=list, default=[]) \
+            .parse_args()
+
+        # Return the created user in json format
+        return dumps(users[user_id], indent=json_indent), 201  # created
+
+    def delete(self, user_id: str = None) -> (str, int):
+        # A user id must be provided to delete a user
         if not user_id:
-            return f"A user id is required to delete a user", 400
+            # If no user id has been provided return a string to prompting the caller to provide user id
+            return dumps({'message': "User id required to delete a user"}, indent=json_indent), 400  # bad request
 
-        deleted_user = users.pop(user_id, None)
+        if not user_id.strip():
+            return dumps({'message': "User id required to delete a user"}, indent=json_indent), 400  # bad request
 
-        if deleted_user is None:
-            return f"Could not find {user_id} to delete", 404
-        return dumps(deleted_user, indent=json_indent), 200
+        # Check if provided user id exists:
+        abort_if_not_exists(user_id, users, f"Cannot find user with user id {user_id}!")
+
+        # Remove and return the user id, None is returned as default value to avoid KeyError
+        return dumps(users.pop(user_id, None), indent=json_indent), 200  # OK
 
 
-api.add_resource(User, "/api/user/", "/api/user/<string:user_id>")
+# Add the class to route
+api.add_resource(Users, "/api/users", "/api/user/<string:user_id>")
 
 
-class ChatRoom(Resource):
+class ChatRooms(Resource):
     # Gets room with room_id.
     # Returns either a key-value pair, or an array of key-value pairs.
     def get(self, room_id=None):
-        if room_id:
-            abort_if_not_exists(room_id, rooms, "Room does not exist")
-            return rooms[room_id], 200
-        return list(rooms.keys()), 200
+        if room_id:  # Check if room id has been provided
+            # Check if room id exists:
+            abort_if_not_exists(room_id, rooms, f"Cannot find room with room id {room_id}!")
+
+            # Return this room in json format
+            return dumps(rooms[room_id], indent=json_indent), 200  # OK
+
+        # As there are potential for a lot of messages in each room we will only return a list of rooms together
+        # With the users in these rooms
+        all_rooms = {k: v['users'] for (k, v) in rooms.items()}
+        return dumps(all_rooms, indent=json_indent), 200  # OK
 
     # Adds room with room_id
     def post(self, room_id=None):
         if not room_id:
-            return f"Cannot create a room without a room id", 400
+            return dumps({'message': "Room id is required to create a new room"}, indent=json_indent), 400
         abort_if_exists(room_id, rooms, "Room already exists")
         rooms[room_id] = reqparse.RequestParser() \
-            .add_argument('name', type=str, required=True) \
-            .add_argument('size', type=int, required=True) \
+            .add_argument('name', type=str, default=room_id) \
             .add_argument('users', type=list, default=[]) \
             .add_argument('messages', type=list, default=[]) \
             .parse_args()
-        return rooms[room_id], 201
+        return dumps(rooms[room_id], indent=json_indent), 201
 
 
-api.add_resource(ChatRoom, "/api/rooms/", "/api/rooms/<string:room_id>")
+api.add_resource(ChatRooms, "/api/rooms/", "/api/room/<string:room_id>")
 
 
 class RoomUsers(Resource):
-    def get(self, room_id):
+    def get(self, room_id: str) -> (str, int):
+        # Check if room_id exists
         abort_if_not_exists(room_id, rooms, f"Unable to find the room with room id {room_id}!")
+
+        # Return json format of all the users in this room
         return dumps(rooms[room_id]['users']), 200
 
-    def post(self, room_id, user_id):
-        abort_if_not_exists(room_id, rooms, f"Cannot find room with room id {room_id}")
-        abort_if_not_exists(user_id, users, f"Cannot find user with user id {user_id}")
-        rooms[room_id]['users'].append(user_id)
-        users[user_id]['rooms'].append(room_id)
-        return dumps(rooms[room_id]['users']), 200
-
-    # Adding a delete request so a user can leave a chat room
-    def delete(self, room_id, user_id):
-        abort_if_not_exists(room_id, rooms, f"Cannot find room with room id {room_id}")
-        abort_if_not_exists(user_id, users, f"Cannot find user with user id {user_id}")
-        # Since every user_id's should be unique we do not need to have any concern when using remove on an array:
-        rooms[room_id]['users'].remove(user_id)
-        users[user_id]['rooms'].remove(room_id)
-        return dumps(rooms[room_id]['users']), 200
-
-
-api.add_resource(RoomUsers, "/api/room/<string:room_id>/users", "/api/room/<string:room_id>/users/<string:user_id>")
-
-
-class RoomMessage(Resource):
-    def get(self, room_id):
+    def post(self, room_id: str) -> (str, int):
+        # Check if room_id exists
         abort_if_not_exists(room_id, rooms, f"Cannot find room with room id {room_id}!")
-        return dumps(rooms[room_id]['messages'], indent=json_indent), 200
+
+        # To join a room append a user to the list of users in that room:
+        # First check if the user is registered:
+        user = reqparse.RequestParser().add_argument('user', type=str, required=True).parse_args()
+        user = user['user']
+        abort_if_not_exists(user, users, f"Cannot find user with user id {user}!")
+
+        # Then check if the user is already in this room
+        abort_if_exists(user, rooms[room_id]['users'], f"{user} is already a member of {room_id}")
+
+        # Add user to users list in this room
+        rooms[room_id]['users'].append(user)
+        # Add room id to rooms list for this user
+        users[user]['rooms'].append(room_id)
+
+        return dumps(rooms[room_id]['users'], indent=json_indent), 200
 
 
-api.add_resource(RoomMessage, "/api/rooms/<room-id>/messages")
+api.add_resource(RoomUsers, "/api/room/<string:room_id>/users")
 
 
-class UserMessage(Resource):
-    def get(self, room_id, user_id):
+class Messages(Resource):  # Take a look at this
+    def get(self, room_id, user_id=None):
+        # Check if bot room_id and user_id exists
         abort_if_not_exists(room_id, rooms, f"Cannot find room with room id {room_id}")
-        abort_if_not_exists(user_id, users, f"Cannot find user with user id {user_id}!")
 
+        if not user_id:  # If no user id was provided
+            # Assume we want to get all messages in this room regardless of users
+            return dumps(rooms[room_id]['messages'], indent=json_indent), 200
+
+        # If a user id was provided we must then first check if the user
+        abort_if_not_exists(user_id, users, f"Cannot find user with user id {user_id}!")
         # Get a list of all messages from a room:
         messages_in_this_room = rooms[room_id]['messages']
+
+        # Filter through all the messages in this room and return all the messages this provided user_id
         messages_from_user = filter(lambda message: message['user'] == user_id, messages_in_this_room)
         return dumps(list(messages_from_user), indent=json_indent), 200
 
     def post(self, room_id, user_id):
-        abort_if_not_exists(room_id, rooms, f"Cannot find room with room id {room_id}")
-        abort_if_not_exists(user_id, users, f"Cannot find user with user id {user_id}!")
+        # Check if room and user exists
+        abort_if_not_exists(room_id, rooms, f"Cannot find room with room id \"{room_id}\"!")
+        abort_if_not_exists(user_id, users, f"Cannot find user with user id \"{user_id}\"!")
+
+        # Also check if the user is member of the room
+        abort_if_not_exists(
+            user_id,
+            rooms[room_id]['users'],
+            f"user \"{user_id}\" cannot send messages in room \"{room_id}\"..."
+        )
 
         now = datetime.now()
         message = reqparse.RequestParser() \
@@ -142,9 +186,9 @@ class UserMessage(Resource):
 
 
 api.add_resource(
-    UserMessage,
-    "api/rooms/<room-id>/<user-id>/messages",
-    "api/rooms/<room-id>/<user-id>/messages/<string:message-id>"
+    Messages,
+    "/api/room/<string:room_id>/messages",
+    "/api/room/<string:room_id>/<string:user_id>/messages"
 )
 
 if __name__ == "__main__":

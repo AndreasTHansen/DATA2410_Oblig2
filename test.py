e5120ace2eeafe59966aef9eb62f12dff75c84dd
uyqn <s341864@oslomet.no>
2021-04-01 15:44:06 +0200

    Small hotfixes

--- test.py
+++ test.py
@@ -195,20 +195,25 @@ join(arguments.room)
 # For this we start a thread:
 def live_messages():
     while True:
-        room = client.recv(1024).decode('utf8')  # Receive the room from socket
-        if active_room == room:  # First check if the room has happened in the active room
-            refresh_messages_in_this_room()
-        elif push_enabled:  # Then check if push has been enabled
-            user_info, c = User.get(active_user)  # Get information about active user:
-            unread_messages = user_info.get('unread-messages', None)  # Get the unread messages
-            if unread_messages is not None:
-                number_of_unread = unread_messages.get(room, None)  # Grab the number in given room
-                if number_of_unread is not None:
-                    print(f"You have {number_of_unread} unread messages in {room}")  # Print notification to the user
+        try:
+            room = client.recv(1024).decode('utf8')  # Receive the room from socket
+            if active_room == room:  # First check if the room has happened in the active room
+                refresh_messages_in_this_room()
+            elif push_enabled:  # Then check if push has been enabled
+                user_info, c = User.get(active_user)  # Get information about active user:
+                unread_messages = user_info.get('unread-messages', None)  # Get the unread messages
+                if unread_messages is not None:
+                    number_of_unread = unread_messages.get(room, None)  # Grab the number in given room
+                    if number_of_unread is not None:
+                        # Print notification to the user
+                        print(f"You have {number_of_unread} unread messages in {room}")
+                    else:
+                        print(f"New activity in {room}")  # In case if we're not able to get the desired number
                 else:
-                    print(f"New activity in {room}")  # In case if we're not able to get the desired number
-            else:
-                print(f"New activity in {room}")  # In case we do not get the desired dictionary
+                    print(f"New activity in {room}")  # In case we do not get the desired dictionary
+        except requests.ConnectionError:
+            print(f"Lost connection to the API server. Exiting the program!")
+            break
 
 
 # We must also be able to send messages:
